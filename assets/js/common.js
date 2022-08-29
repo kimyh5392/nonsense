e65f61423de709e19d96b4aeafca906d3ecc87ec
Kim Yong Hwan <mk.kimyh@gmail.com>
2022-08-29 11:59:43 +0900

    메인 퍼블리싱 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,4 +6,13 @@
     $(document).on('click', '.btn-nav-close', function(){
         $('body').removeClass('opened');
     });
+    
+    // header scroll
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 0) {
+            $('body').addClass('scrolled');
+        } else {
+            $('body').removeClass('scrolled');
+        }
+    });
 })(jQuery);
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
 <link rel="stylesheet" href="/assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 </head>
 <body>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
@@ -47,12 +48,309 @@
                 </p>
             </div>
         </article>
+        <!-- about -->
+        <section class="about effect">
+            <div class="container">
+                <div class="section-header effect">
+                    <h3>About CI</h3>
+                    <figure>
+                        <img src="/assets/images/main/img_logo.png" alt="">
+                    </figure>
+                </div>
+                <div class="section-content effect">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/main/img_about01.png" alt="">
+                                <figcaption>
+                                    <b>We’re called<br> ‘Nonsense’</b>
+                                    <p>Forget normal</p>
+                                </figcaption>
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/main/img_about02.png" alt="">
+                                <figcaption>
+                                    <b>We Propose<br> Extraordinary Ideas</b>
+                                    <p>Fresh and exciting ideas around<br> universally loved themes</p>
+                                </figcaption>
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/main/img_about03.png" alt="">
+                                <figcaption>
+                                    <b>We Recharge your<br> Content Batteries</b>
+                                    <p>For markets with a thirst for new</p>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- work -->
+        <section class="work effect">
+            <div class="container">
+                <div class="section-header">
+                    <h3>Our Work</h3>
+                </div>
+                <div class="section-content">
+                    <ul class="work-slider">
+                        <li>
+                            <div class="slide-content">
+                                <div class="figure">
+                                    <img src="/assets/images/main/img_work_slide01.png" alt="">
+                                </div>
+                                <div class="details">
+                                    <dl>
+                                        <dt>
+                                            Entertainment<br> format planning
+                                        </dt>
+                                        <dd>
+                                            Planning various entertainment <br>
+                                            formats such as game shows, <br>
+                                            reality shows, and talk shows
+                                        </dd>
+                                    </dl>
+                                    <a href="#" class="btn-more"></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="slide-content">
+                                <div class="figure">
+                                    <img src="/assets/images/main/img_work_slide02.png" alt="">
+                                </div>
+                                <div class="details">
+                                    <dl>
+                                        <dt>
+                                            Short-form<br> content planning
+                                        </dt>
+                                        <dd>
+                                            Planning various entertainment<br>
+                                            formats such as game shows, <br>
+                                            reality shows, and talk shows
+                                        </dd>
+                                    </dl>
+                                    <a href="#" class="btn-more"></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="slide-content">
+                                <div class="figure">
+                                    <img src="/assets/images/main/img_work_slide03.png" alt="">
+                                </div>
+                                <div class="details">
+                                    <dl>
+                                        <dt>Drama / <br> Movie Planning</dt>
+                                        <dd>
+                                            Drama and movie planning<br>
+                                            based on entertainment /<br>
+                                            comedy elements
+                                        </dd>
+                                    </dl>
+                                    <a href="#" class="btn-more"></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="slide-content">
+                                <div class="figure">
+                                    <img src="/assets/images/main/img_work_slide04.png" alt="">
+                                </div>
+                                <div class="details">
+                                    <dl>
+                                        <dt>
+                                            other additional <br> business
+                                        </dt>
+                                        <dd>
+                                            Various content <br>
+                                            planning regardless of <br>
+                                            any format
+                                        </dd>
+                                    </dl>
+                                    <a href="#" class="btn-more"></a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- ceo-info -->
+        <article class="ceo-info effect">
+            <div class="container">
+                <div class="v-align">
+                    <h3>Beom Dong, Sohn</h3>
+                    <dl>
+                        <dt>CEO</dt>
+                        <dd>Format Creator</dd>
+                    </dl>
+                    <p>
+                        <i>
+                            “I dream that one day our <br>
+                            formats are going to<br class="d-block">
+                            make the world happier.”
+
+                        </i>
+                    </p>
+                </div>
+            </div>
+        </article>
+        <!-- leaders -->
+        <section class="leaders effect">
+            <div class="container">
+                <div class="section-header">
+                    <h3>Slate Leaders</h3>
+                </div>
+                <div class="section-content">
+                    <ul class="slate-slider">
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider01.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Talent in the Kitchen</dt>
+                                            <dd>
+                                                Welcome to the world’s first cooking meets talent show. <br>
+                                                Because keeping kids happy at mealtimes is hard!
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider02.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Celebrity Location Hunter</dt>
+                                            <dd>
+                                                2 teams, 24 hours to find the perfect location. The winning team will star in the scene itself. <br>
+                                                A competitive travel show that also uncovers some of a country’s most beautiful and undiscovered hotspots.
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider03.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Hidden DJ</dt>
+                                            <dd>
+                                                A studio entertainment format where music afficionados battle to find the soundtrack to some of life’s most emotional and memorable moments 
+                                                <br>- that then take place on stage….
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider04.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Love Drive Thru</dt>
+                                            <dd>
+                                                Singles flock to our dating precinct where they are matched up by AI and taken on a date by an autonomous car. But will their journey end in love?  This is First Dates of the future!
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider01.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Talent in the Kitchen</dt>
+                                            <dd>
+                                                Welcome to the world’s first cooking meets talent show. <br>
+                                                Because keeping kids happy at mealtimes is hard!
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider02.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Celebrity Location Hunter</dt>
+                                            <dd>
+                                                2 teams, 24 hours to find the perfect location. The winning team will star in the scene itself. <br>
+                                                A competitive travel show that also uncovers some of a country’s most beautiful and undiscovered hotspots.
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider03.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Hidden DJ</dt>
+                                            <dd>
+                                                A studio entertainment format where music afficionados battle to find the soundtrack to some of life’s most emotional and memorable moments 
+                                                <br>- that then take place on stage….
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/main/img_slate_slider04.png" alt="">
+                                    <figcaption>
+                                        <dl>
+                                            <dt>Love Drive Thru</dt>
+                                            <dd>
+                                                Singles flock to our dating precinct where they are matched up by AI and taken on a date by an autonomous car. But will their journey end in love?  This is First Dates of the future!
+                                            </dd>
+                                        </dl>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="controls">
+                        <a href="javascript:;" class="btn-prev"></a>
+                        <a href="javascript:;" class="btn-next"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script>
         (function($){
+            // animation 관련
             setTimeout(function(){
                 $('.jumbotron').addClass('action');
             }, 500);
@@ -66,6 +364,34 @@
                         $(elem).addClass('action');
                     }
                 });
+            });
+            
+            // work-slider
+            $('.work-slider').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 4000,
+            });
+            
+            // slate-slider
+            $('.slate-slider').slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+                prevArrow: $('.btn-prev'),
+                nextArrow: $('.btn-next'),
+                autoplay: true,
+                autoplaySpeed: 4000,
+                responsive: [{
+                breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }]
             });
         })(jQuery);
     </script>
