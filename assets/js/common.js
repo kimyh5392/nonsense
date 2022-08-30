8e1207752cd134c849d61eff5d5439e78f0bd089
Kim Yong Hwan <mk.kimyh@gmail.com>
2022-08-30 12:29:13 +0900

    gnb 메뉴 클릭시 활성화 event 처리 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,25 +10,29 @@
         $('body').removeClass('opened');
     });
     
-    // header scroll
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() > 0) {
-            $('body').addClass('scrolled');
-        } else {
-            $('body').removeClass('scrolled');
-        }
-    });
     
     // header gnb menu click event
     var $link = $('#gnb > ul > li > a, .nav-content ul > li > a');
     
     $link.on('click',function(e){
         var target = $($(this).attr('href')); 
+        $('#gnb > ul > li').removeClass('active');
+        target.parent().addClass('active');
         $('html, body').animate({
             scrollTop: target.offset().top - 75
         }, 400);
         $(this).parent().addClass('active');
         $('body').removeClass('opened');
         e.preventDefault();
+    });
+    
+    // header scroll
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 0) {
+            $('body').addClass('scrolled');
+        } else {
+            $('body').removeClass('scrolled');
+            $link.parent().removeClass('active');
+        }
     });
 })(jQuery);
